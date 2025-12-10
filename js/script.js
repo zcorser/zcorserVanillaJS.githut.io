d17f62e86d2f066d6cba4fef18f04f789ec811ec
Zoe Corser <zcorser@umich.edu>
2025-12-11 00:59:42 +0100

    VanillaJS final submission

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-const addNumbers = (first, second) => {
-  return first + second;
-}
 
-if (typeof module !== 'undefined') {
-  module.exports = {
-    addNumbers
-  }
-}
+document.querySelector('#click-me').addEventListener('click', () => {
+  document.querySelectorAll('h1').forEach((h1) => h1.innerHTML = 'You clicked that button!!!');
+  document.querySelector('body').style.backgroundColor = 'red' ;
+  document.querySelectorAll('div.hidden').forEach((div) => {
+    div.style.display = 'block';
+  });
+});
 
